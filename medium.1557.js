d3bce71abce1b8e4faccbf7dd41fb6978f074509
simolex <simolex@yandex.ru>
2023-05-22 17:10:16 +0300

    save resolved problem

---
+++ medium.1557.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+const canPlaceFlowers = function (flowerbed, n) {
+    let i = 0;
+    const LengthFlowerbed = flowerbed.length;
+    while (i < LengthFlowerbed && n > 0) {
+        if (flowerbed[i] === 0) {
+            if (flowerbed[i + 1] === 0) {
+                n -= 1;
+                i += 2;
+            }
+        } else {
+            i += 2;
+        }
+    }
+    return n < 1;
+};
+
+canPlaceFlowers([1, 0, 0, 0, 1], 2);
