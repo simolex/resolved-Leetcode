0c208092bd58695c65f6a5f65f594dfde2e835fe
simolex <simolex@yandex.ru>
2024-12-03 21:22:47 +0300

    done task 394

--- medium/0x.js/medium.0394.js
+++ medium/0x.js/medium.0394.js
@@ -5,16 +5,44 @@
  * @return {string}
  */
 var decodeString = function (s) {
-    let backets = [];
     let current;
     const regDigit = /[0-9]+/;
-    return s.split("").reduce((stack, t) => {
-        if (regDigit.test(t)) {
-            current = stack.pop();
-            if (regDigit.test(current)) {
-            }
-        } else {
-        }
-        return stack;
-    }, []);
+    return s
+        .split("")
+        .reduce(
+            (stack, t) => {
+                if (regDigit.test(t)) {
+                    current = stack.pop();
+                    if (!current) {
+                        stack.push(t);
+                    } else if (regDigit.test(current)) {
+                        stack.push(current + t);
+                    } else {
+                        stack.push(current);
+                        stack.push(t);
+                    }
+                } else if (t === "[") {
+                    stack.push("[", "");
+                } else if (t === "]") {
+                    let str = "";
+                    let part;
+                    while ((part = stack.pop()) != "[") {
+                        str = part + str;
+                    }
+                    const count = stack.pop();
+                    stack.push(str.repeat(+count));
+                } else {
+                    stack.push(stack.pop() + t);
+                }
+                return stack;
+            },
+            [""]
+        )
+        .join("");
 };
+
+console.log(decodeString("3[a]2[bc]"));
+console.log(decodeString("3[a2[c]]"));
+console.log(decodeString("2[abc]3[cd]ef"));
+console.log(decodeString("3[a2[c]]"));
+console.log(decodeString("abc3[cd]xyz"));
