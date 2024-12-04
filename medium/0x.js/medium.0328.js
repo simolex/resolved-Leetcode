b64e0f101e1f16623e1287703472aa8e7bc49569
simolex <simolex@yandex.ru>
2024-12-04 21:47:03 +0300

    done task 328

--- medium/0x.js/medium.0328.js
+++ medium/0x.js/medium.0328.js
@@ -13,22 +13,51 @@
  * @return {ListNode}
  */
 var oddEvenList = function (head) {
-    let even;
-    const odd = head;
-    let num = 1;
-    while (head) {
-        if (num % 2 == 1) {
-            if (even) {
-                even.next = head.next;
-                even = even.next;
-            } else {
-                even = head.next;
-            }
+    const result = head;
+    let even = null;
+    let evenHead = null;
 
-            head.next = even.next;
-            even.next = undefined;
-        }
-
-        num++;
+    let currentEven = null;
+    let isOdd = true;
+    if (!head) {
+        return head;
     }
+
+    while (head.next != null) {
+        if (isOdd) {
+            //вырезаем четный элемент
+            currentEven = head.next;
+            if (currentEven !== null) {
+                head.next = currentEven.next;
+                currentEven.next = null;
+            } else {
+                head.next = null;
+            }
+            //формируем цепочку четных
+            if (even === null) {
+                even = currentEven;
+                evenHead = even;
+            } else {
+                even.next = currentEven;
+                even = even.next;
+            }
+        } else {
+            head = head.next;
+        }
+        isOdd = !isOdd;
+    }
+    //прицепляем четные в конце списка
+    head.next = evenHead;
+
+    return result;
 };
+
+//тестирование
+function ListNode(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+}
+
+const a = [2, 1, 3, 5, 6, 4, 7].reverse().reduce((prev, v) => new ListNode(v, prev), undefined);
+console.dir(a, { depth: null });
+console.dir(oddEvenList(a), { depth: null });
