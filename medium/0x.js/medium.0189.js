4e597fbedd5115c62c36924ee4fbf83c1d5e9246
simolex <simolex@yandex.ru>
2024-12-04 22:54:42 +0300

    done task 189

---
+++ medium/0x.js/medium.0189.js
@@ -0,0 +1,22 @@
+/**
+ * 189. Rotate Array
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+    const n = nums.length;
+    k = k % n;
+    const rotated = new Array(n).fill(0);
+
+    for (let i = 0; i < n; i++) {
+        rotated[(i + k) % n] = nums[i];
+    }
+
+    for (let i = 0; i < n; i++) {
+        nums[i] = rotated[i];
+    }
+};
+
+console.log(rotate([1, 2, 3, 4], 2));
