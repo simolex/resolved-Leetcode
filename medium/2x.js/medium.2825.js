ab5e5b0abf5a738f1660088524eaa79a3df34564
simolex <simolex@yandex.ru>
2024-12-04 13:40:13 +0300

    done task 2825

--- medium/2x.js/medium.2825.js
+++ medium/2x.js/medium.2825.js
@@ -6,12 +6,13 @@
  * @return {boolean}
  */
 var canMakeSubsequence = function (str1, str2) {
-    const dict = "abcdefghijklmnopqrstuvwxyz";
     const cyclic = new Map();
+    const dict = "abcdefghijklmnopqrstuvwxyz";
+
     for (let i = 0; i < dict.length; i++) {
         cyclic.set(dict[i], dict[(i + 1) % dict.length]);
     }
-    // console.log(cyclic);
+
     let j = 0;
     for (let i = 0; i < str1.length; i++) {
         if (str1[i] === str2[j] || cyclic.get(str1[i]) === str2[j]) {
