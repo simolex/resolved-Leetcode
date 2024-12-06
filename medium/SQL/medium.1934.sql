98360fab50e9bba74c31f292cf5e9b672676938e
simolex <simolex@yandex.ru>
2024-12-06 14:08:24 +0300

    done task 1934

---
+++ medium/SQL/medium.1934.sql
@@ -0,0 +1,16 @@
+/**
+ * 1934. Confirmation Rate
+ */
+
+ SELECT s.user_id,
+	ROUND(
+		AVG(
+			CASE 
+    			WHEN c.action = 'confirmed' THEN 1
+    			ELSE 0
+    		END), 
+        2) confirmation_rate 
+FROM signups s 
+	LEFT JOIN  confirmations c 
+    ON s.user_id = c.user_id
+GROUP BY s.user_id 
