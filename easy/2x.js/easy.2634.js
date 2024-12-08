21cde3a68848663de5f1c2d59a2511eb4e101ae5
simolex <simolex@yandex.ru>
2024-12-08 22:06:52 +0300

    done task 2634

---
+++ easy/2x.js/easy.2634.js
@@ -0,0 +1,14 @@
+/**
+ * 2634. Filter Elements from Array
+ *
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function (arr, fn) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i, arr)) result.push(arr[i]);
+    }
+    return result;
+};
