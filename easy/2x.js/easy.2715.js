31bb388bf992ecd17537e983d2f753ff1b56949d
simolex <simolex@yandex.ru>
2024-12-09 15:22:04 +0300

    done task 2715

--- easy/2x.js/easy.2715.js
+++ easy/2x.js/easy.2715.js
@@ -13,26 +13,26 @@ var cancellable = function (fn, args, t) {
     };
 };
 
-const result = [];
+// const result = [];
 
-const fn = (x) => x * 5;
-const args = [2],
-    t = 100,
-    cancelTimeMs = 50;
+// const fn = (x) => x * 5;
+// const args = [2],
+//     t = 100,
+//     cancelTimeMs = 50;
 
-const start = performance.now();
+// const start = performance.now();
 
-const log = (...argsArr) => {
-    const diff = Math.floor(performance.now() - start);
-    result.push({ time: diff, returned: fn(...argsArr) });
-};
+// const log = (...argsArr) => {
+//     const diff = Math.floor(performance.now() - start);
+//     result.push({ time: diff, returned: fn(...argsArr) });
+// };
 
-const cancel = cancellable(log, args, t);
+// const cancel = cancellable(log, args, t);
 
-const maxT = Math.max(t, cancelTimeMs);
+// const maxT = Math.max(t, cancelTimeMs);
 
-setTimeout(cancel, cancelTimeMs);
+// setTimeout(cancel, cancelTimeMs);
 
-setTimeout(() => {
-    console.log(result); // [{"time":20,"returned":10}]
-}, maxT + 15);
+// setTimeout(() => {
+//     console.log(result); // [{"time":20,"returned":10}]
+// }, maxT + 15);
