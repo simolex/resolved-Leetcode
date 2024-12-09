19c121ba17d27ea8ed365d0eb02e09b5ac7a5711
simolex <simolex@yandex.ru>
2024-12-09 15:49:44 +0300

    done task 2637

---
+++ medium/2x.js/medium.26.37.js
@@ -0,0 +1,20 @@
+/**
+ * 2637. Promise Time Limit
+ *
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function (fn, t) {
+    return async function (...args) {
+        return new Promise((res, rej) => {
+            setTimeout(rej, t, "Time Limit Exceeded");
+            fn(...args).then(res, rej);
+        });
+    };
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
