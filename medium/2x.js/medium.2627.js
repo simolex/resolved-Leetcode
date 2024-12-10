1cc348e3831ac59b68d43181e1e93c0897ffb058
simolex <simolex@yandex.ru>
2024-12-10 11:42:20 +0300

    done task 2627

---
+++ medium/2x.js/medium.2627.js
@@ -0,0 +1,21 @@
+/**
+ * 2627. Debounce
+ *
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function (fn, t) {
+    let timer = null;
+    return function (...args) {
+        clearTimeout(timer);
+        timer = setTimeout(fn, t, ...args);
+    };
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
