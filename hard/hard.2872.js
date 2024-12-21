e4ac26caeac397233875edca7fba6494be4881e8
simolex <simolex@yandex.ru>
2024-12-21 20:35:57 +0300

    done task 2872

--- hard/hard.2872.js
+++ hard/hard.2872.js
@@ -8,65 +8,50 @@
  * @return {number}
  */
 var maxKDivisibleComponents = function (n, edges, values, k) {
+    if (n == 1) return 1;
+
     const graph = new Map();
     const stack = [];
     const visited = Array(n).fill(false);
-    const neighborsCount = Array(n).fill(0);
-    const getLeaves = () => {
-        for (let i = 0; i < neighborsCount.length; i++) {
-            if (neighborsCount[i] <= 1 && !visited[i]) {
-                stack.push(i);
-            }
-        }
-        return;
-    };
-    edges.forEach((edge) => {
-        if (!graph.has(edge[0])) {
-            graph.set(edge[0], []);
-        }
-        graph.get(edge[0]).push(edge[1]);
-        neighborsCount[edge[0]]++;
+    const parent = Array(n).fill(null);
+    let countComponents = 0;
 
-        if (!graph.has(edge[1])) {
-            graph.set(edge[1], []);
+    edges.forEach(([u, v]) => {
+        if (!graph.has(u)) {
+            graph.set(u, []);
         }
-        graph.get(edge[1]).push(edge[0]);
-        neighborsCount[edge[1]]++;
+        if (!graph.has(v)) {
+            graph.set(v, []);
+        }
+
+        graph.get(v).push(u);
+        graph.get(u).push(v);
     });
 
     let current;
-    let pntFront = 0;
-    let parent;
-    let rest;
-    let count = 0;
-    getLeaves();
-    while (pntFront < stack.length) {
-        const last = stack.length;
-        while (pntFront < last) {
-            current = stack[pntFront];
+    stack.push(0);
+    while (stack.length) {
+        current = stack.pop();
+
+        if (!visited[current]) {
             visited[current] = true;
-            pntFront++;
-
-            if (values[current] % k === 0) {
-                count++;
-                rest = 0;
-            } else {
-                rest = values[current];
-            }
-
-            for (let j = 0; j < graph.get(current).length; j++) {
-                parent = graph.get(current)[j];
-                if (!visited[parent]) {
-                    stack.push(parent);
-                    neighborsCount[parent]--;
-                    values[parent] += rest;
+            stack.push(current);
+            graph.get(current).forEach((v) => {
+                if (!visited[v]) {
+                    parent[v] = current;
+                    stack.push(v);
                 }
+            });
+        } else {
+            if (values[current] % k === 0) {
+                countComponents++;
+            } else {
+                values[parent[current]] += values[current];
             }
         }
     }
-    console.log(graph);
-    console.log(neighborsCount);
-    console.log(count);
+
+    return countComponents;
 };
 
 console.log(
@@ -82,3 +67,21 @@ console.log(
         6
     )
 );
+
+console.log(
+    maxKDivisibleComponents(
+        7,
+        [
+            [0, 1],
+            [0, 2],
+            [1, 3],
+            [1, 4],
+            [2, 5],
+            [2, 6]
+        ],
+        [3, 0, 6, 1, 5, 2, 1],
+        3
+    )
+);
+
+console.log(maxKDivisibleComponents(1, [], [3], 3));
