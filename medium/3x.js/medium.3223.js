035e1c039d7e76a2b5f5cb4094cb90120d89c225
simolex <simolex@yandex.ru>
2025-01-13 09:04:18 +0300

    done task 3223 ver.1

---
+++ medium/3x.js/medium.3223.js
@@ -0,0 +1,22 @@
+/**
+ * 3223. Minimum Length of String After Operations
+ *
+ * @param {string} s
+ * @return {number}
+ */
+var minimumLength = function (s) {
+    const freqLetters = s.split("").reduce((hashSet, l) => {
+        if (!hashSet.has(l)) {
+            hashSet.set(l, { cnt: 0 });
+        }
+        hashSet.get(l).cnt++;
+        return hashSet;
+    }, new Map());
+
+    return [...freqLetters.values()]
+        .map(({ cnt }) => cnt)
+        .reduce((restLength, c) => restLength + (c > 2 ? ((c - 1) % 2) + 1 : c), 0);
+};
+
+console.log(minimumLength("abaacbcbb"));
+console.log(minimumLength("aa"));
