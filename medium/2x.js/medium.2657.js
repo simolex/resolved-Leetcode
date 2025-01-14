5542cef002e6f8a3d83205e424cb17ce0c52c265
simolex <simolex@yandex.ru>
2025-01-14 12:34:17 +0300

    done task 2657

---
+++ medium/2x.js/medium.2657.js
@@ -0,0 +1,33 @@
+/**
+ * 2657. Find the Prefix Common Array of Two Arrays
+ *
+ * @param {number[]} A
+ * @param {number[]} B
+ * @return {number[]}
+ */
+var findThePrefixCommonArray = function (A, B) {
+    const n = A.length;
+    const patternA = Array(n).fill(false);
+    const patternB = Array(n).fill(false);
+
+    const countCompare = () => {
+        let count = 0;
+        for (let i = 0; i < n; i++) {
+            if (patternA[i] && patternB[i]) {
+                count++;
+            }
+        }
+        return count;
+    };
+
+    const result = [];
+    for (let i = 0; i < n; i++) {
+        patternA[A[i] - 1] = true;
+        patternB[B[i] - 1] = true;
+        result.push(countCompare());
+    }
+    return result;
+};
+
+console.log(findThePrefixCommonArray([1, 3, 2, 4], [3, 1, 2, 4]));
+console.log(findThePrefixCommonArray([2, 3, 1], [3, 1, 2]));
