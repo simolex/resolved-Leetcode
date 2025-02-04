83bee888cc0fed7daba40ff653563ad7b81cb4f0
simolex <simolex@yandex.ru>
2025-02-04 16:19:32 +0300

    done task 70

---
+++ easy/0x.js/easy.0070.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+    const steps = [1, 1];
+
+    for (let i = 2; i <= n; i++) {
+        steps[i] = steps[i - 1] + steps[i - 2];
+    }
+    return steps[n];
+};
