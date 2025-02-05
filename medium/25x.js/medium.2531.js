31aedffb2777a8792e3ed61d76dd61d3374ccfb7
simolex <simolex@yandex.ru>
2025-02-05 15:47:56 +0300

    save 2531

---
+++ medium/25x.js/medium.2531.js
@@ -0,0 +1,16 @@
+/**
+ * 2531. Make Number of Distinct Characters Equal
+ *
+ * @param {string} word1
+ * @param {string} word2
+ * @return {boolean}
+ */
+var isItPossible = function (word1, word2) {
+    for (let c of word1) {
+        console.log(c);
+    }
+
+    // const freqWord1 = word1.split("")
+};
+
+console.log(isItPossible("ac", "b"));
