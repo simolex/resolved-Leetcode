6845433d0950b791cb0169907c02034b9a9e3c6c
simolex <simolex@yandex.ru>
2025-02-10 21:20:37 +0300

    done task 46

--- medium/00x.js/medium.0046.js
+++ medium/00x.js/medium.0046.js
@@ -6,42 +6,63 @@
  */
 var permute = function (nums) {
     let n = nums.length;
-    const indecies = Array.from({ length: n }, (_, i) => i);
-    const count = indecies.reduce((f, v) => f * (v + 1), 1);
+    const count = Array(n)
+        .fill()
+        .reduce((f, _, i) => f * (i + 1), 1);
     const result = [];
+
     let left, right;
+    nums.sort((a, b) => a - b);
 
     for (let i = 0; i < count; i++) {
         result.push([...nums]);
 
         left = n - 2;
-        while (indecies[left] > indecies[left + 1] && left >= 0) {
+        while (left >= 0 && nums[left] >= nums[left + 1]) {
             left--;
         }
 
         right = n - 1;
-        while (indecies[right] < indecies[left] && left >= 0) {
+        while (left >= 0 && nums[left] >= nums[right]) {
             right--;
         }
 
-        [nums[indecies[left]], nums[indecies[right]]] = [
-            nums[indecies[right]],
-            nums[indecies[left]],
-        ];
+        [nums[left], nums[right]] = [nums[right], nums[left]];
 
         left++;
-        right++;
-
+        right = n - 1;
         while (left < right) {
-            [nums[indecies[left]], nums[indecies[right]]] = [
-                nums[indecies[right]],
-                nums[indecies[left]],
-            ];
+            [nums[left], nums[right]] = [nums[right], nums[left]];
             left++;
             right--;
         }
     }
-    console.log(result);
+    return result;
+};
+
+var permute = function (nums) {
+    let n = nums.length;
+    const result = [];
+    let mask = 0;
+    let currentState = Array(n);
+
+    const nextDigit = (index = 0) => {
+        if (index === n) {
+            result.push([...currentState]);
+            return;
+        }
+
+        for (let bit = 1, i = 0; i < n; i++, bit <<= 1) {
+            if (mask & bit) continue;
+            mask |= bit;
+            currentState[index] = nums[i];
+            nextDigit(index + 1);
+            mask &= ~bit;
+        }
+    };
+
+    nextDigit();
+    return result;
 };
 
 console.log(permute([1, 2, 3]));
