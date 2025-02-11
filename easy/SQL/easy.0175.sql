a7abfa1c06fe0644d00d3133e41284642a1d26cb
simolex <simolex@yandex.ru>
2025-02-11 23:35:23 +0300

    done task 175 (fix)

--- easy/SQL/easy.0175.sql
+++ easy/SQL/easy.0175.sql
@@ -1,7 +1,11 @@
 /**
  * 175. Combine Two Tables
  */
-SELECT p.firstname, p.lastname, a.city, a.state
-from person p 
+SELECT 
+    p.firstname, 
+    p.lastname, 
+    a.city, 
+    a.state
+FROM person p 
 	 LEFT JOIN address a 
-    	on a.personid = p.personid
+    	ON a.personid = p.personid
