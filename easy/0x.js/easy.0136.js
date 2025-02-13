eceab2402395caf826885bcbf857d3a0df5b42a7
simolex <simolex@yandex.ru>
2025-02-14 00:47:30 +0300

    done task 136

---
+++ easy/0x.js/easy.0136.js
@@ -0,0 +1,19 @@
+/**
+ * 136. Single Number
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+    let result = 0;
+
+    for (let num of nums) {
+        result ^= num;
+    }
+
+    return result;
+};
+
+console.log(singleNumber([2, 2, 1]));
+console.log(singleNumber([4, 1, 2, 1, 2]));
+console.log(singleNumber([1]));
