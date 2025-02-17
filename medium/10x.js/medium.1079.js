c27cf8dbefde8207736ac971e8a603e4884f22d5
simolex <simolex@yandex.ru>
2025-02-17 23:02:15 +0300

    done task 1079

--- medium/10x.js/medium.1079.js
+++ medium/10x.js/medium.1079.js
@@ -4,17 +4,39 @@
  * @param {string} tiles
  * @return {number}
  */
-const fact = [0, 1];
+const fact = [1, 1];
 for (let e = 2; e <= 7; e++) {
     fact[e] = fact[e - 1] * e;
 }
 
 var numTilePossibilities = function (tiles) {
-    let n = tiles.length;
     const tileMap = {};
     for (let tile of tiles) {
         tileMap[tile] = -~tileMap[tile];
     }
+
+    const tileMapValues = Object.values(tileMap);
+
+    const nextLetter = (maxLength, nextKey = 0, current = fact[maxLength]) => {
+        if (maxLength === 0) {
+            return current;
+        }
+
+        let count = 0;
+        for (let i = 0; i <= tileMapValues[nextKey]; i++) {
+            if (maxLength - i < 0) {
+                continue;
+            }
+            count += nextLetter(maxLength - i, nextKey + 1, current / fact[i]);
+        }
+        return count;
+    };
+
+    let countPossibilities = 0;
+    for (let i = 1; i <= tiles.length; i++) {
+        countPossibilities += nextLetter(i);
+    }
+    return countPossibilities;
 };
 
 console.log(numTilePossibilities("AAB"));
