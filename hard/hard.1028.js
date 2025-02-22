0adddd5692db00e0ed596bacdcc2fd19a2819327
simolex <simolex@yandex.ru>
2025-02-22 13:06:06 +0300

    done task 1028

---
+++ hard/hard.1028.js
@@ -0,0 +1,47 @@
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+}
+/**
+ * 1028. Recover a Tree From Preorder Traversal
+ *
+ * @param {string} traversal
+ * @return {TreeNode}
+ */
+var recoverFromPreorder = function (traversal) {
+    let res, depth, node, root;
+    const regNode = /([-]*)([0-9]+)/g;
+    const stack = [];
+
+    res = regNode.exec(traversal);
+    root = new TreeNode(Number(res[2]));
+    stack.push(0, root);
+
+    res = regNode.exec(traversal);
+    while (stack.length && res !== null) {
+        node = stack.pop();
+        depth = stack.pop();
+
+        if (res[1].length == depth + 1) {
+            if (!node.left) {
+                node.left = new TreeNode(Number(res[2]));
+
+                stack.push(depth, node);
+                stack.push(depth + 1, node.left);
+            } else {
+                node.right = new TreeNode(Number(res[2]));
+
+                stack.push(depth + 1, node.right);
+            }
+
+            res = regNode.exec(traversal);
+        }
+    }
+    return root;
+};
+
+console.log(recoverFromPreorder("1-2--3--4-5--6--7"));
+console.log(recoverFromPreorder("1-2--3---4-5--6---7"));
+console.log(recoverFromPreorder("1-401--349---90--88"));
