c0ceda1b49b327800e121fceae3091137c3fab60
simolex <simolex@yandex.ru>
2025-03-22 23:04:19 +0300

    done task 2685

--- medium/26x.js/medium.2685.js
+++ medium/26x.js/medium.2685.js
@@ -90,6 +90,35 @@ var countCompleteComponents = function (n, edges) {
     return count;
 };
 
+// Version #3
+var countCompleteComponents = function (n, edges) {
+    const bitsSet = Array.from({ length: n }, (_, i) => Math.pow(2, i));
+
+    const hash = Array.from(bitsSet);
+    const bitsCount = Array(n).fill(0);
+    for (let [u, v] of edges) {
+        hash[u] += bitsSet[v];
+        hash[v] += bitsSet[u];
+
+        bitsCount[u]++;
+        bitsCount[v]++;
+    }
+
+    const freq = new Map();
+    const counting = new Map();
+    for (let i = 0; i < n; i++) {
+        counting.set(hash[i], (counting.get(hash[i]) || 0) + bitsCount[i]);
+        freq.set(hash[i], -~freq.get(hash[i]));
+    }
+
+    let count = 0;
+    for (let [sum, cnt] of freq.entries()) {
+        if (cnt * (cnt - 1) === counting.get(sum)) count++;
+    }
+
+    return count;
+};
+
 console.log(
     countCompleteComponents(6, [
         [0, 1],
@@ -105,5 +134,52 @@ console.log(
         [1, 2],
         [3, 4],
         [3, 5]
+    ])
+);
+console.log(
+    countCompleteComponents(37, [
+        [27, 20],
+        [28, 20],
+        [17, 20],
+        [7, 17],
+        [35, 28],
+        [1, 27],
+        [12, 7],
+        [3, 35],
+        [32, 17],
+        [29, 12],
+        [24, 17],
+        [31, 7],
+        [4, 12],
+        [11, 25],
+        [26, 25],
+        [5, 11],
+        [16, 26],
+        [13, 5],
+        [2, 25],
+        [8, 2],
+        [10, 26],
+        [23, 10],
+        [23, 13],
+        [5, 10],
+        [8, 5],
+        [2, 11],
+        [6, 9],
+        [0, 9],
+        [19, 6],
+        [15, 19],
+        [34, 6],
+        [33, 19],
+        [36, 0],
+        [21, 0],
+        [14, 9],
+        [30, 15],
+        [18, 19],
+        [22, 33],
+        [0, 33],
+        [34, 18],
+        [9, 19],
+        [19, 36],
+        [9, 22]
     ])
 );
