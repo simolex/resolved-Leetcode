e84ba2920d23708101dce054ec6ca6c53b7ecbf7
simolex <simolex@yandex.ru>
2025-03-25 19:35:16 +0300

    add DSU to libs

---
+++ _libs/DSU.js
@@ -0,0 +1,27 @@
+class DSU {
+    parent;
+    weight;
+    constructor(n) {
+        this.parent = Array.from({ length: n }, (_, i) => i);
+        this.weight = Array(n).fill(1);
+    }
+
+    find(v) {
+        return this.parent[v] === v ? v : (this.parent[v] = this.find(this.parent[v]));
+    }
+
+    union(a, b) {
+        a = this.find(a);
+        b = this.find(b);
+        if (this.weight[a] > this.weight[b]) {
+            [a, b] = [b, a];
+        }
+        this.weight[b] += this.weight[a];
+        this.parent[a] = b;
+    }
+
+    size(v) {
+        v = this.find(v);
+        return this.weight[v];
+    }
+}
