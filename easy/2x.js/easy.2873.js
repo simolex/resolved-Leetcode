d0f7f810819b28e1be48c10efb25cebb4ece52ba
simolex <simolex@yandex.ru>
2025-04-02 07:48:23 +0300

    done task 2873

---
+++ easy/2x.js/easy.2873.js
@@ -0,0 +1,37 @@
+/**
+ * 2873. Maximum Value of an Ordered Triplet I
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumTripletValue = function (nums) {
+    let n = nums.length;
+    let max = -Infinity;
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            for (let k = j + 1; k < n; k++) {
+                max = Math.max(max, (nums[i] - nums[j]) * nums[k]);
+            }
+        }
+    }
+    return max < 0 ? 0 : max;
+};
+
+var maximumTripletValue = function (nums) {
+    let n = nums.length;
+    let maxT = -Infinity;
+
+    let maxI;
+    for (let k = 2; k < n; k++) {
+        maxI = nums[0];
+        for (let j = 1; j < k; j++) {
+            maxT = Math.max(maxT, (maxI - nums[j]) * nums[k]);
+            maxI = Math.max(maxI, nums[j]);
+        }
+    }
+    return maxT < 0 ? 0 : maxT;
+};
+
+console.log(maximumTripletValue([12, 6, 1, 2, 7]));
+console.log(maximumTripletValue([1, 10, 3, 4, 19]));
+console.log(maximumTripletValue([1, 2, 3]));
