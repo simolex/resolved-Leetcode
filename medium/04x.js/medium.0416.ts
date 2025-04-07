86ee74789ea9fc2b5e1c07bbbaad523f60df1810
simolex <simolex@yandex.ru>
2025-04-07 12:24:53 +0300

    done task 416

---
+++ medium/04x.js/medium.0416.ts
@@ -0,0 +1,32 @@
+/**
+ * 416. Partition Equal Subset Sum
+ *
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canPartition = function (nums: number[]) {
+    let fullSum = 0;
+    for (let num of nums) {
+        fullSum += num;
+    }
+
+    if (fullSum % 2 !== 0) {
+        return false;
+    }
+    const halfSum = fullSum / 2;
+
+    const dp: boolean[] = Array(halfSum + 1).fill(false);
+    dp[0] = true;
+
+    for (let num of nums) {
+        for (let curSum = halfSum; curSum >= num; curSum--) {
+            dp[curSum] ||= dp[curSum - num];
+        }
+        if (dp[halfSum]) return true;
+    }
+
+    return dp[halfSum];
+};
+
+console.log(canPartition([1, 5, 11, 5]));
+console.log(canPartition([1, 2, 3, 5]));
